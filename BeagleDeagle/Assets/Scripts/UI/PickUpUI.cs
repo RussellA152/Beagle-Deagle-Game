b0b50e518b21585d6de9e0257490790bc4e58e1b
RussellA152 <russellayala152@gmail.com>
2024-01-16 21:41:03 -0800

    Power up descriptions are now queued

--- BeagleDeagle/Assets/Scripts/UI/PickUpUI.cs
+++ BeagleDeagle/Assets/Scripts/UI/PickUpUI.cs
@@ -10,12 +10,18 @@ public class PickUpUI : MonoBehaviour
 
     [SerializeField] private TMP_Text textBox;
 
+    [SerializeField, Range(1f, 5f)] 
+    private float durationOfText;
+
+    private bool _textIsCurrentlyDisplayed;
+
     private void OnEnable()
     {
         playerEvents.displayPlayerPickUpDescription += DisplayPickUpText;
 
         // Hide text on enable
         textBox.text = string.Empty;
+        _textIsCurrentlyDisplayed = false;
     }
 
     private void OnDisable()
@@ -28,6 +34,24 @@ public class PickUpUI : MonoBehaviour
     /// Ex. Player retrieves a speed boost pick up, and sees "Increased Movement and Attack Speed!"
     private void DisplayPickUpText(string description)
     {
+        StartCoroutine(DisplayPickUpForDuration(description));
+    }
+
+    private IEnumerator DisplayPickUpForDuration(string description)
+    {
+        while (_textIsCurrentlyDisplayed)
+            yield return null;
+        
         textBox.text = description;
+
+        _textIsCurrentlyDisplayed = true;
+        
+        yield return new WaitForSeconds(durationOfText);
+
+        textBox.text = string.Empty;
+
+        _textIsCurrentlyDisplayed = false;
+
+
     }
 }
